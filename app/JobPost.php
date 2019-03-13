1ef2f2139cf5869ca090cde3eae2d6c429acd0fc
shima <shima.k@formsdotstar.com>
2019-03-13 12:52:48 +0530

    Jobpost message here

--- app/JobPost.php
+++ app/JobPost.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class JobPost extends Model
 {
-    protected $fillable = ['post', 'code','department'];
+    protected $fillable = ['post', 'code','department','session','postNo','description','desirable','startDate','startingTime','closeDate','closingTime','status','uAcadmic','uTeachExp','uResExp','uPreDetails','uNoc','uResArt','uResPub','created_by','updated_by'];
     protected $table = 'jobpost';
 
     public function department()
